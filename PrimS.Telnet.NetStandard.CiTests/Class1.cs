f11d244a982d6b3765ec978d19cbe47552d21fa6
9swampy <github@prims.com>
2017-02-15 20:00:58 +0000

    Upgrade FluentAssertions to 4.19.0 so can add same version to NetStandard.CiTests. NetStandardCiTests compiling without error but if I add "testrunner": "mstest" then it throws detecting tests. Commit before trying to consume xunit instead.

---
+++ PrimS.Telnet.NetStandard.CiTests/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace PrimS.Telnet.NetStandard.CiTests
+{
+  using System;
+  using System.Linq;
+  using FluentAssertions;
+  using System.Threading.Tasks;
+  using System.Text.RegularExpressions;
+  using System.Diagnostics.CodeAnalysis;
+  using Telnet.CiTests;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+  [TestClass]
+  public class WithClient
+  {
+    private const int TimeoutMs = 100;
+
+    [TestMethod]
+    public void ShouldConnect()
+    {
+      using (TelnetServer server = new TelnetServer())
+      {
+        using (Client client = new Client(server.IPAddress.ToString(), server.Port, new System.Threading.CancellationToken()))
+        {
+          client.IsConnected.Should().Be(true);
+        }
+      }
+    }
+  }
+}
